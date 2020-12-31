2f76ea9cb2380d98e0ef5e8f5f14fb14a6c77c99
Henrik Lagrosen <henrik@Henriks-MacBook-Air.local>
2020-12-31 21:06:19 +0100

    bugfix

--- scripts/googleDrive.js
+++ scripts/googleDrive.js
@@ -28,7 +28,7 @@ function initClient() {
         authorizeButton.onclick = handleAuthClick;
         signoutButton.onclick = handleSignoutClick;
     }, function(error) {
-        appendPre(JSON.stringify(error, null, 2));
+        console.log(JSON.stringify(error, null, 2));
     });
 }
 function updateSigninStatus(isSignedIn) {
